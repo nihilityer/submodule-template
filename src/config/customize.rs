8d4568d5d7755cde74276d088a5594766c3bcbac
nihilityer <hch2796945700@outlook.com>
2024-03-25 19:50:34 +0800

    feat: 初步加入配置初始化功能

--- src/config/customize.rs
+++ src/config/customize.rs
@@ -1,1 +1,6 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Deserialize, Serialize, Default, Debug, Clone)]
+pub struct CustomizeConfig {
+    pub flag: bool,
+}
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,1 +1,32 @@
+use nihility_common::SubmoduleInfo;
+use serde::{Deserialize, Serialize};
+use crate::config::customize::CustomizeConfig;
+
 mod customize;
+
+#[derive(Deserialize, Serialize, Default, Debug, Clone)]
+pub struct SubmoduleConfig {
+    pub submodule_name: String,
+    pub core_public_key_path: String,
+    pub default_receiver_submodule: String,
+    pub info: SubmoduleInfo,
+    pub log_config: SubmoduleLogConfig,
+    pub instruct_server_config: InstructServerConfig,
+    pub manipulate_server_config: ManipulateServerConfig,
+    pub customize_config: CustomizeConfig,
+}
+
+#[derive(Deserialize, Serialize, Default, Debug, Clone)]
+pub struct InstructServerConfig {
+
+}
+
+#[derive(Deserialize, Serialize, Default, Debug, Clone)]
+pub struct ManipulateServerConfig {
+
+}
+
+#[derive(Deserialize, Serialize, Default, Debug, Clone)]
+pub struct SubmoduleLogConfig {
+
+}
